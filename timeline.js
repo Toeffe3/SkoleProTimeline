262550c83c22bf5f063dec896ac4ca0f8238e906
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2018-04-04 09:55:08 +0200

    Done - almost

--- timeline.js
+++ timeline.js
@@ -1,9 +1,7 @@
 console.log(data.events.length);
-var tlbox = {};
 
 function setup() {
-  createCanvas(1400,600);
-
+  createCanvas(1500,800);
 
   stroke(255);
   strokeWeight(3);
@@ -12,7 +10,6 @@ function setup() {
   stroke(0);
   strokeWeight(2);
   var size = width / data.events.length - .1;
-
 }
 
 function draw() {
@@ -25,21 +22,31 @@ function hover() {
   background(0);
   for (var i = 0; i < data.events.length; i++) {
     if (mouseX >= i*size && mouseY >= height/2-5 && mouseX <= i*size+size && mouseY <= height/2-5+10) {
+      fill(42, 200, 255);
+      rect(i*size, height/2-5, size, 10);
+      if(i*size >= width - 250) {
+        rect(width-250, 20, 250, 150);
+        fill(255);
+        noStroke();
+        text(data.events[i].Desc, width-250+4, 20+4, 250-4, 150-4);
+      } else {
+        rect(i*size, 20, 250, 150);
+        fill(255);
+        noStroke();
+        text(data.events[i].Desc, i*size+4, 24, 250-4, 150-4);
+      }
 
-      fill(255,0,0);
-      tlbox[i] = rect(i*size, height/2-5, size, 10);
     } else {
-      fill(255-(i*20),112-(i*20),19);
-      tlbox[i] = rect(i*size, height/2-5, size, 10);
+      fill(0+(i*10),112+(i*10),19);
+      rect(i*size, height/2-5, size, 10);
     }
     h++;
     fill(255);
     textSize(10);
     textStyle(NORMAL);
-    text(data.events[i].Year, i*size, height/2-(h%2*25)+8, size, 10);
+    text(data.events[i].Year, i*size, height/2-(h%2*25)+8, size*2, 10);
     textSize(12);
     textStyle(BOLD);
-    text(data.events[i].Title, i*size, height/2-(h%2*50)+19, size, 12);
+    text(data.events[i].Title, i*size, height/2-(h%2*50)+19, size*2, 12);
   }
-
 }
