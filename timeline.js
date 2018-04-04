4352446739ecc614b073d708320abc7664525f71
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2018-04-04 08:39:26 +0200

    More text

--- timeline.js
+++ timeline.js
@@ -1,10 +1,9 @@
 console.log(data.events.length);
 var tlbox = {};
-var h = 0;
 
 function setup() {
   createCanvas(1400,600);
-  background(0);
+
 
   stroke(255);
   strokeWeight(3);
@@ -18,22 +17,28 @@ function setup() {
 
 function draw() {
   hover();
-
 }
 
 function hover() {
+  var h = 0;
   var size = width / data.events.length - .1;
+  background(0);
   for (var i = 0; i < data.events.length; i++) {
     if (mouseX >= i*size && mouseY >= height/2-5 && mouseX <= i*size+size && mouseY <= height/2-5+10) {
+
       fill(255,0,0);
       tlbox[i] = rect(i*size, height/2-5, size, 10);
     } else {
       fill(255-(i*20),112-(i*20),19);
       tlbox[i] = rect(i*size, height/2-5, size, 10);
-      h++;
-      fill(255);
-
-      text(i, height/2+(h%2*10), data.events[i].Year)
     }
+    h++;
+    fill(255);
+    stroke(255);
+    textSize(10);
+    console.log(data.events[i].Year);
+    text(data.events[i].Year, i*size, height/2-10+(h%2)*30, size, 10);
+    text(data.events[i].Title, i*size, height/2-10+(h%2)*40, size, 10);
   }
+
 }
